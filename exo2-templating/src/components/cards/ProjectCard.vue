79c794333df6f40ce72ab8e397cf77bc6bb73f12
Maxime C <forgeoffdev@gmail.com>
2025-01-21 10:27:07 +0100

    .

---
+++ exo2-templating/src/components/cards/ProjectCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import IconStarPlusOutline from "@/components/icons/cards/iconStarPlusOutline.vue";
+import IconEye from "@/components/icons/cards/iconEye.vue";
+import IconMerge from "@/components/icons/cards/iconMerge.vue";
+
+defineProps<{ title: string; content: string; }>()
+</script>
+
+<template>
+  <div class="card">
+    <h5>{{title}}</h5>
+    <p>{{content}}</p>
+    <div class="project-buttons">
+      <icon-star-plus-outline />
+      <icon-eye />
+      <icon-merge />
+    </div>
+  </div>
+</template>
